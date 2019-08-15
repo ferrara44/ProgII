a0a8c7029b9359f1c3ae40160f6ea7d0c7d6bc57
Facundo Ferrara <facundo@42mate.com>
2019-08-15 18:49:09 -0300

    Ejercicio 2

--- src/TP2/Ejercicio1.java
+++ src/TP2/Ejercicio1.java
@@ -6,17 +6,35 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		int numberarray[]= new int[10];
-		int pos, neg, zero=0;
-		Scanner integers = new Scanner(System.in);
+		int pos=0, neg=0, zero=0;
+		Scanner integerscan = new Scanner(System.in);
 		
-		System.out.printf("Please enter 10 integers./n");
+		System.out.println("Please enter 10 integers.");
 		
-		for(int i=0; i<10; i++) {
-			System.out.println("Enter integer for the position "+i);
-			numberarray[i]=integers.nextInt();
-			System.out.printf(numberarray[i]);
+		for(int i=0 ; i<10 ; i++) {
+			System.out.println("Enter integer for the position "+(i+1));
+			numberarray[i]=integerscan.nextInt();
+			
 		}
 		
+		//for(int i=0 ; i<10 ; i++) {
+		//	System.out.print(numberarray[i]+" ");
+		//}
+		
+		for(int i=0 ; i<10 ; i++) {
+			if(numberarray[i]==0) {
+				zero++;
+			}
+			else if(numberarray[i]>0){
+				pos++;
+			}
+			else {
+				neg++;
+			}
+		}
+		System.out.println("Your entered values array contain");
+		System.out.println(zero+" occurrences of 0");
+		System.out.println(pos+" occurrences of positive values.");
+		System.out.println(neg+" occurrences of negative values.");
 	}
-	
 }
---
+++ src/TP2/Ejercicio2.java
@@ -0,0 +1,31 @@
+package TP2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		
+		int numberarray[]= new int[10];
+		float acu=0;
+		Scanner integerscan = new Scanner(System.in);
+		
+		System.out.println("Please enter 10 integers.");
+		
+		for(int i=0 ; i<10 ; i++) {
+			System.out.println("Enter integer for the position "+(i));
+			numberarray[i]=integerscan.nextInt();
+		}
+		
+		//for(int i=0 ; i<10 ; i++) {
+		//	System.out.print(numberarray[i]+" ");
+		//}
+		
+		for(int i=0 ; i<10 ; i++) {
+			if(i%2==0) { //Debes utilizar el operador módulo “%”
+				acu+=numberarray[i];
+				System.out.println(acu);
+			}
+		}
+		System.out.println(acu/5);
+	}
+}
