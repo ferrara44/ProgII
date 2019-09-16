32e6344caf4e9555c8c8a0760bd6b7d84f221182
Facundo Ferrara <facundo@42mate.com>
2019-09-15 22:38:11 -0300

    Updated Exercises

--- src/TP6/Generate.java
+++ src/TP6/Generate.java
@@ -1,13 +1,13 @@
 package TP6;
 
-public class Generate {
+public class Generate{
 
-    private int cantcar;
-    String passwords = "";
+	String passwords = "";
+    private int characters;
 
     public Generate(int chosenLength){
-        this.cantcar = chosenLength;
-        for (int j = 0; j<chosenLength; j++){
+        this.characters = chosenLength;
+        for (int i = 0; i<chosenLength; i++){
             switch((int) Math.floor(Math.random()*(3))) {
                 case 0: this.passwords += genNumber();   break;
                 case 1: this.passwords += genLowercase();   break;
@@ -15,16 +15,18 @@ public class Generate {
             }
         }
     }
-     
-    public static char genLowercase(){
-        return (char) Math.floor(Math.random() * (26) + 97);
-    }
     
-    public static char genUppercase(){
+    //Generators and Getters
+    
+    public char genUppercase(){
         return (char) Math.floor(Math.random()*(26) + 65);
     }
     
-    public static int genNumber(){
+    public char genLowercase(){
+        return (char) Math.floor(Math.random() * (26) + 97);
+    }
+    
+    public int genNumber(){
         return (char) Math.floor(Math.random()*(10));
     }
     
@@ -33,18 +35,18 @@ public class Generate {
     }
     
     public int getLength(){
-        return this.cantcar;
+        return this.characters;
     }
     
     public boolean getSafety(){
-        int numbers=0 , uppercase=0 , lowercase=0;
-        for (int i=0;   i<cantcar;  i++){
-            if (Character.isDigit(passwords.charAt(i)))numbers+=1;
-            else if (Character.isUpperCase(passwords.charAt(i)))uppercase+=1;
-            else lowercase+=1;
+        int uppercase=0, lowercase=0,numbers=0;
+        for (int i=0 ; i<characters ; i++){
+            if (Character.isUpperCase(passwords.charAt(i)))numbers+=1;
+            else if (Character.isLowerCase(passwords.charAt(i)))uppercase+=1;
+            else numbers+=1;
         }
         
-        if (numbers>5 && lowercase>1 && uppercase>2){
+        if (lowercase>1 && uppercase>2 && numbers>5){
             return true;
         }
         else return false;
--- src/TP6/Passwords.java
+++ src/TP6/Passwords.java
@@ -1,32 +1,32 @@
-
 package TP6;
 
 import java.util.Scanner;
 
-public class Passwords {
+public class Passwords{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Please select amount of passwords to generate:");
         int amount = scan.nextInt();
-        System.out.println("Select password lenght: ");
-        int cant =scan.nextInt();
+        System.out.println("Select password lenght");
+        int length = scan.nextInt();
         Generate[] password = new Generate[amount];
 
-        for(int i = 0 ; i < amount ; i++) {
-            password[i] = new Generate(cant);
+        for(int i=0 ; i<amount ; i++) {
+            password[i] = new Generate(length);
         }
+        
         System.out.println("The following Passwords have been generated:");
-        for (int i=0;   i<amount; i++){
+        
+        
+        for (int i=0 ; i<amount ; i++){ //Prints the password then returns it's safety level.
         	System.out.print(password[i].getPass()+ password[i].getLength());
             if (password[i].getSafety()){
                 System.out.println("\t Safe");
-            }else   System.out.println("\tUnsafe"); //looks nicer this way. Imo.
-
+            }
+            else   System.out.println("\tUnsafe");
         }
-
         scan.close();
     }
-
 }
