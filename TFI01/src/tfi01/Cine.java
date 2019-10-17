706c3f9c8c2693a4049ac86887e7b6f83709801a
ferraraphys@gmail.com <kartdei@Facundo.Lenovo>
2019-10-17 12:24:29 -0300

    Trabajo Integrador

--- TFI01/src/tfi01/Cine.java
+++ TFI01/src/tfi01/Cine.java
@@ -1,10 +1,27 @@
-
 package tfi01;
+
+import java.util.Scanner;
 
 /**
  *
  * @author kartdei
  */
+
 public class Cine {
     
+    Scanner scan = new Scanner(System.in);
+    
+    
+    public void cargarEspectadores (){
+        System.out.println("Nombre:");
+        String nombre = scan.next();
+        System.out.println("Edad:");
+        int edad = scan.nextInt();
+        System.out.println("Fila:");
+        String fila = scan.next();
+        System.out.println("Silla:");
+        int silla = scan.nextInt();
+        
+        Espectadores Espectador = new Espectadores(nombre,edad, fila, silla);
+    }
 }
