afe9a51a1d264ffc2f7ff0f217700d34ad784ab1
ferraraphys@gmail.com <kartdei@Facundo.Lenovo>
2019-10-17 02:36:41 -0300

    Setup for final project.

--- TFI01/src/tfi01/TFI01.java
+++ TFI01/src/tfi01/TFI01.java
@@ -7,7 +7,5 @@ package tfi01;
 public class TFI01 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("vamos menem");
     }
 }
