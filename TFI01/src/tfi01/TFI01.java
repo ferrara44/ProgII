03713609dfac58320d0d66c151d78430cc8670ed
ferrara44 <kartdei@Facundo.Lenovo>
2019-10-17 02:33:49 -0300

    Setup for final project.

---
+++ TFI01/src/tfi01/TFI01.java
@@ -0,0 +1,13 @@
+
+package tfi01;
+
+/**
+ * @author Facundo Ferrara
+ */
+public class TFI01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("vamos menem");
+    }
+}
