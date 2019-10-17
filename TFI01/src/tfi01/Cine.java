d546fd8219ab9d4461c951cef0b148c07509885e
ferraraphys@gmail.com <kartdei@Facundo.Lenovo>
2019-10-17 12:37:41 -0300

    Trabajo Integrador

--- TFI01/src/tfi01/Cine.java
+++ TFI01/src/tfi01/Cine.java
@@ -11,8 +11,8 @@ public class Cine {
     
     Scanner scan = new Scanner(System.in);
     
-    
     public void cargarEspectadores (){
+
         System.out.println("Nombre:");
         String nombre = scan.next();
         System.out.println("Edad:");
@@ -21,7 +21,12 @@ public class Cine {
         String fila = scan.next();
         System.out.println("Silla:");
         int silla = scan.nextInt();
-        
+        //Al pedir nextInt, el programa finalizará si no recibe un entero.
+
         Espectadores Espectador = new Espectadores(nombre,edad, fila, silla);
     }
+    
+
+    
+    Salas sala01 = new Salas(3,"sala01");
 }
